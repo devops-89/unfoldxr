f8b30862a11118a9b3622135fc9677dd6097b990
Akashhgupta-dev <akash.gupta.digixito@gmail.com>
2026-04-07 15:54:30 +0530

    last chages

--- components/layouts/home/HeroSection.tsx
+++ components/layouts/home/HeroSection.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import PageHeroSection from "@/components/widgets/PageHeroSection";
 import { homePage } from "@/utils/Website-Data";
 import banner from "@/images/banner/homePage/hero.png";
+import { useDemoModal } from "@/components/context/DemoModalContext";
 
 const HeroSection = () => {
   const data = homePage.herosection;
+  const { openModal } = useDemoModal();
 
   return (
     <PageHeroSection
@@ -12,7 +16,7 @@ const HeroSection = () => {
       title={data.heading2}
       description={data.description}
       primaryBtn={{ label: data.cta1 }}
-      secondaryBtn={{ label: data.cta2 }}
+      secondaryBtn={{ label: data.cta2, onClick: openModal }}
     />
   );
 };
